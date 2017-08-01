fda987838916d18fa49b18ffd75ce83499104d61
Anthony Damico <ajdamico@gmail.com>
2017-08-01 08:27:11 -0400

    75% of meps

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-lodown( "meps" , output_dir = file.path( getwd() ) )
+
+meps_cat <-
+	get_catalog( "meps" ,
+		output_dir = file.path( getwd() ) )
+
+# sample 75% of the records
+which_records <- sample( seq( nrow( meps_cat ) ) , round( nrow( meps_cat ) * 0.75 ) )
+
+# always sample year == 2014
+meps_cat <- unique( rbind( meps_cat[ which_records , ] , subset( meps_cat , year == 2014 ) ) )
+
+lodown( "meps" , meps_cat )

5e3b6e68ef11114caf9fb719a4f158f14f8ee36f
Anthony Damico <ajdamico@gmail.com>
2017-08-29 08:19:17 -0400

    meps replicates

--- tests/setup.R
+++ tests/setup.R
@@ -1,15 +1,4 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-
-meps_cat <-
-	get_catalog( "meps" ,
-		output_dir = file.path( getwd() ) )
-
-# sample 75% of the records
-which_records <- sample( seq( nrow( meps_cat ) ) , round( nrow( meps_cat ) * 0.75 ) )
-
-# always sample year == 2014
-meps_cat <- unique( rbind( meps_cat[ which_records , ] , subset( meps_cat , year %in% c( "2014" , "1996-2014" ) ) ) )
-
-lodown( "meps" , meps_cat )
+lodown( "meps" , output_dir = file.path( getwd() ) )

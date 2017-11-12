117180138253987334c559aab737a53aea50caaf
Anthony Damico <ajdamico@gmail.com>
2017-11-12 03:15:13 -0500

    fixes https://github.com/ajdamico/asdfree/issues/308

--- tests/setup.R
+++ tests/setup.R
@@ -1,15 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
 library(lodown)
 
 meps_cat <-
 	get_catalog( "meps" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 75% of the records
-which_records <- sample( seq( nrow( meps_cat ) ) , round( nrow( meps_cat ) * 0.75 ) )
+record_categories <- ceiling( seq( nrow( meps_cat ) ) / ceiling( nrow( meps_cat ) / 2 ) )
 
-# always sample year == 2015
-meps_cat <- unique( rbind( meps_cat[ which_records , ] , subset( meps_cat , grepl( 2015 , year ) ) ) )
+meps_cat <- unique( rbind( meps_cat[ record_categories == this_sample_break , ] , meps_cat[ grepl( 2015 , meps_cat$year ) , ] ) )
 
 lodown( "meps" , meps_cat )

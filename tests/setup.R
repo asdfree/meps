57272c89aa69e4e6faa76c7429bdd1cfb9544d3f
Anthony Damico <ajdamico@gmail.com>
2018-01-08 07:47:24 -0500

    library(lodown)

--- tests/setup.R
+++ tests/setup.R
@@ -1,5 +1,6 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+library(lodown)
 this_sample_break <- Sys.getenv( "this_sample_break" )
 meps_cat <- get_catalog( "meps" , output_dir = file.path( getwd() ) )
 record_categories <- ceiling( seq( nrow( meps_cat ) ) / ceiling( nrow( meps_cat ) / 2 ) )
